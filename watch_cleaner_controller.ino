3b2898aca7cb9b5b09bdf3dc8fd632d8fc673622
rtcw50 <johnhrusso@gmail.com>
2023-10-28 19:13:02 -0500

    Update watch_cleaner_controller.ino
    Redesign with lookup tables for the timer and motor speed.
    Now uses for loop for motor fwd/rev cycle.

--- watch_cleaner_controller.ino
+++ watch_cleaner_controller.ino
@@ -7,7 +7,7 @@
 #define DRV8871_IN1 7
 #define DRV8871_IN2 8
 
-#define STBY_RUN 4
+#define SET_RUN 4
 #define A_MOTORSPEED 5
 #define A_RUNTIME 6
 
@@ -17,24 +17,44 @@
 //#define RUNTIME 15000
 #define WAITTIME 10
 #define RUNTIME 15000
+//#define RUNTIME 150
 
 #define ACCEL_DECEL 30
 
+#define LAST_LIT_LED_INDEX 2
 
-byte progressBar[PROGRESSBARWIDTH];
-int timerSetting;
+// Lookup Tables indexed by A/D results
+// Both A/Ds are 4-bit resolution (0-15)
+// Time A/D sets the duration of the cleaning period
+// Speed A/D sets the RPM of the motor 
+
+// Time A/D Lookups
+// The LED display bar bits, indexed from the timer A/D pin value
+byte progressBarLookup[16] = 
+  {0x0,0x0,0x1,0x1,0x3,0x3,0x7,0x7,0xF,0xF,0x1F,0x1F,0x3F,0x3F,0x7F,0xFF};
+// Cleaning period duration, indexed from the timer A/D pin value
+int timerSettingLookup[16] =
+  {0,1,2,3,4,5,6,7,8,8,8,8,8,8,8,8 };
+// Motor speed setting (PWM value) indexed from the Speed A/D pin
+int motorSpeedLookup[16] = 
+  {64,64,96,96,128,128,160,160,192,192,224,224,240,240,255,255};
+
+// Value 0-15, tracks the progress of the clean, 
 int currentProgress;
-int cleaningCycleCount;
+// The cleaning timer looked up in timerSettingLookup and indexed by the timer A/D
+int timerSetting;
+// The motor speed PWM value from motorSpeedLookup and indexed by the motor speed A/D
 int motorSpeed;
+// ncleanCycles is the number of forward/reverse spin cycles 
+// timerSetting * <number>
+int ncleanCycles;
+// True: the motors are on, False: motors off
 int Working;
+
+int Debug=0;
 
 void setup() {
   // Shift Register Setup
-  // Create an array of size PROGRESSBARWIDTH
-  // and init it to a bit field
-  // i.e. [0] = 0b00000001
-  //      [1] = 0b00000011
-  // etc.
   pinMode(ShiftRegDataPin, OUTPUT);
   pinMode(ShiftRegLatchPin, OUTPUT);
   pinMode(ShiftRegClockPin, OUTPUT);
@@ -46,51 +66,49 @@ void setup() {
   digitalWrite(DRV8871_IN2, LOW);
 
   // Standby/Run Pin, set to HIGH to run.
-  pinMode(STBY_RUN, INPUT);
+  pinMode(SET_RUN, INPUT);
   
   // Run time analog input set up
   pinMode(A_RUNTIME, INPUT);
-  // 3-bit (0-7) analog in resolution
-  analogReadResolution(3);
+  // 4-bit (0-15) analog in resolution
+  analogReadResolution(4);
 
   // Motor speed analog input set up
   pinMode(A_MOTORSPEED, INPUT);
-  //analogReadResolution(8);
 
   
   // Globals initialization
-  cleaningCycleCount = 10;
+  ncleanCycles = 5;
   Working = 0;
 
-  int ledStatus = 1;
-
-  for (int i=0; i<PROGRESSBARWIDTH; i++) {
-    progressBar[i] = ledStatus;
-    ledStatus = ledStatus | (ledStatus << 1);
-  }
-  setProgress(7);
-  Serial.print("setup\n");
+  setProgress(0);
+  if (Debug) { Serial.print("setup\n"); }
 }
 
 void setProgress(int v)
 {
-  currentProgress = v;
+  currentProgress = timerSettingLookup[v];
   digitalWrite(ShiftRegLatchPin, LOW);
-  shiftOut(ShiftRegDataPin, ShiftRegClockPin, MSBFIRST, progressBar[v]);
+  shiftOut(ShiftRegDataPin, ShiftRegClockPin, MSBFIRST, progressBarLookup[v]);
   digitalWrite(ShiftRegLatchPin, HIGH);
 }
+
+int getProgress()
+{
+  return currentProgress;
+}
+
 void updateProgress()
 {
-    Serial.print("Update progress:");
-    currentProgress--;
-    // Wrap around
-    if (currentProgress < 0) {
-      currentProgress = 7;
+    if (Debug) { Serial.print("Update progress:"); }
+    if (currentProgress > 0) {
+      currentProgress--;
     }
-    Serial.print(currentProgress, DEC);
-    Serial.print("\n");
+    if (Debug) {
+      Serial.print(currentProgress, DEC); Serial.print("\n");
+    }
     digitalWrite(ShiftRegLatchPin, LOW);
-    shiftOut(ShiftRegDataPin, ShiftRegClockPin, MSBFIRST, progressBar[currentProgress]);
+    shiftOut(ShiftRegDataPin, ShiftRegClockPin, MSBFIRST, progressBarLookup[currentProgress]);
     digitalWrite(ShiftRegLatchPin, HIGH);
     
 }
@@ -120,95 +138,80 @@ void wait(unsigned long t, unsigned int amt)
   }
 }
 
-
-
 void loop() {
+  int timerIndex, motorSpeedIndex;
   unsigned long currTime = millis();
-  int run = digitalRead(STBY_RUN);
-  run = run | digitalRead(STBY_RUN);
-  Serial.print("In loop\n");
-  Serial.print("Run state: \n"); Serial.print(run,DEC); Serial.print("\n");
+
+  // Read the set/run switch
+  int run = digitalRead(SET_RUN);
+  delay(20);
+  run = run && digitalRead(SET_RUN);
+  if (Debug) {
+    Serial.print("In loop\n");
+    Serial.print("Run state: \n"); Serial.print(run,DEC); Serial.print("\n");
+  }
 
   // Set up motor run time
   if (run == LOW) {
-    Serial.print("Run is low\n");
-    timerSetting = analogRead(A_RUNTIME); // 0-7
-    Serial.print(timerSetting, DEC); Serial.print("\n");
-    setProgress(timerSetting);
+    if (Debug) { Serial.print("Run is low\n"); }
+    timerIndex= analogRead(A_RUNTIME);
+    timerSetting = timerSettingLookup[timerIndex];
+ 
+    if (Debug) { Serial.print(timerSetting, DEC); Serial.print("\n"); }
+    setProgress(timerIndex);
     Working = 1;
-    cleaningCycleCount = (timerSetting + 1) * 7;
   }
   
   if (run == HIGH && (Working == 1)) {
-    Serial.print("Run is high, working is set\n");
-    Serial.print("cleaningCycleCount: "); Serial.print(cleaningCycleCount, DEC); Serial.print("\n");
+    if (Debug) {
+      Serial.print("Run is high, working is set\n");
+      Serial.print("ncleanCycles: "); Serial.print(ncleanCycles, DEC); Serial.print("\n");
+    }
 
-    motorSpeed = analogRead(A_MOTORSPEED) * 35;
-    Serial.print("Motor speed: ");  Serial.print(motorSpeed, DEC); Serial.print("\n");
+    // clean for ncleanCycles
+    for (int i=0; i<ncleanCycles; i++) {
+      // Check set/run switch
+      run = digitalRead(SET_RUN);
+      run = run | digitalRead(SET_RUN);
+
+      // Check set/run switch
+      if (run == LOW) {
+        setProgress(0);
+        Working = 0;
+        break;
+      }
+
+      // Update motorSpeed at the beginning of each spin cycle
+      motorSpeedIndex = analogRead(A_MOTORSPEED) ; 
+      motorSpeed = motorSpeedLookup[motorSpeedIndex];
+      if (Debug) { 
+        Serial.print("Motor speed: ");  Serial.print(motorSpeed, DEC); Serial.print("\n");
+      }
   
-    motorOn(DRV8871_IN1, DRV8871_IN2);
-    wait(currTime, RUNTIME);
-    currTime = millis();
-    //Serial.print("Motor off at: "); Serial.print(currTime, DEC); Serial.print("\n");
-    motorStop(DRV8871_IN1, DRV8871_IN2);
+      motorOn(DRV8871_IN1, DRV8871_IN2);
+      wait(currTime, RUNTIME);
+      currTime = millis();
+      motorStop(DRV8871_IN1, DRV8871_IN2);
     
-    currTime = millis();
-    wait(currTime, WAITTIME);
-    //Serial.print("Rev Motor on at: "); Serial.print(currTime, DEC); Serial.print("\n");
+      currTime = millis();
+      wait(currTime, WAITTIME);
     
-    motorOn(DRV8871_IN2, DRV8871_IN1);
-    currTime = millis();
-    wait(currTime, RUNTIME);
-    //Serial.print("Rev Motor off at: "); Serial.print(currTime, DEC); Serial.print("\n");
+      motorOn(DRV8871_IN2, DRV8871_IN1);
+      currTime = millis();
+      wait(currTime, RUNTIME);
 
-    motorStop(DRV8871_IN2, DRV8871_IN1);
-    currTime = millis();
-    wait(currTime, WAITTIME);
+      motorStop(DRV8871_IN2, DRV8871_IN1);
+      currTime = millis();
+      wait(currTime, WAITTIME);
+    }
     
-    // Count down the cleaning cycles
-    cleaningCycleCount--;
 
     // Decrement the progress display
-    if (cleaningCycleCount % 7 == 0) {
-      updateProgress();
-    }
-        // Finish the clean process
-    if ( cleaningCycleCount < 0 ) {
+    updateProgress();
+        // Finish the clean process, when LEDs are all off (progressBar[0,1]), finish
+    if ( getProgress() < LAST_LIT_LED_INDEX ) {
       Working = 0;
     }
   }
 
-}
-
-
-
-
-void testProgressBar()
-{
-    for (int i = 0; i < PROGRESSBARWIDTH; i++) {
-    digitalWrite(ShiftRegLatchPin, LOW);
-
-    shiftOut(ShiftRegDataPin, ShiftRegClockPin, MSBFIRST, progressBar[i]);
-    digitalWrite(ShiftRegLatchPin, HIGH);
-    delay(500);
-  }
-  // Clear all LEDs
-  digitalWrite(ShiftRegLatchPin, LOW);
-  shiftOut(ShiftRegDataPin, ShiftRegClockPin, MSBFIRST, 0);
-  digitalWrite(ShiftRegLatchPin, HIGH);
-  delay(500);
-
-  // From all lighted to none lighted
-  for (int i = PROGRESSBARWIDTH-1; i >= 0; i--) {
-    digitalWrite(ShiftRegLatchPin, LOW);
-
-    shiftOut(ShiftRegDataPin, ShiftRegClockPin, MSBFIRST, progressBar[i]);
-    digitalWrite(ShiftRegLatchPin, HIGH);
-    delay(500);
-  }
-// Clear all LEDs
-  digitalWrite(ShiftRegLatchPin, LOW);
-  shiftOut(ShiftRegDataPin, ShiftRegClockPin, MSBFIRST, 0);
-  digitalWrite(ShiftRegLatchPin, HIGH);
-  delay(500);
 }

1270f568c73b1e257f2fa8319311ec923945346f
John Russo <johnhrusso@gmail.com>
2025-10-29 17:36:52 -0500

    main buttons created for UI
    Simplified UI has two buttons on the main screen, a start/stop button and a clean/spin button. Both buttons are checked buttons, meaning that toggle their state when clicked. Start and stop starts and stops either the clean or spin mode. A status label is place at the bottom of the main screen.

--- watch_cleaner_controller.ino
+++ watch_cleaner_controller.ino
@@ -15,6 +15,9 @@
 //#include <examples/lv_examples.h>
 //#include <demos/lv_demos.h>
 // Used in lv_indev.c in LVGL library
+
+
+#define WCC_VER "2.0.0" 
 #define FIX_LVINDEV_ROTATION
 #define CALIBRATION_FILE "/TouchCalData1"
 #define REPEAT_CAL false
@@ -49,14 +52,35 @@
 #define DRAW_BUF_SIZE (TFT_HOR_RES * TFT_VER_RES / 10 * (LV_COLOR_DEPTH / 8))
 uint32_t draw_buf[DRAW_BUF_SIZE / 4];
 
-// Apparently the canonical screen width and height (always 240x320 for this TFT display)
+/* Create the TFT_eSPI object used for touch calibration.
+   The TFT_eSPI object is always created with the canonical
+   screen width and height (240x320 for ILI9341).
+ */
 TFT_eSPI tft = TFT_eSPI(SCREEN_WIDTH, SCREEN_HEIGHT); 
 
-// Global GUI Objects
+#if 0
+/* Global Widget Objects */
 lv_obj_t * btn1; // button 1 (ON)
-lv_obj_t * on_off_label;
 lv_obj_t * slider; // slider
 lv_obj_t * sl_label; // slider label
+#endif
+lv_obj_t * mach_status_label;
+
+/* Create reusable on/off button styles */
+static lv_style_t on_button_style;  // button appearance when not clicked
+static lv_style_t off_button_style; // button appearance when clicked
+
+static void set_screen_bg_style(lv_obj_t * scr)
+{
+  static lv_style_t style;
+  lv_style_init(&style);
+  lv_style_set_radius(&style, 5);
+
+  /*Make a gradient*/
+  lv_style_set_bg_opa(&style, LV_OPA_COVER);
+  lv_style_set_bg_color(&style, lv_palette_lighten(LV_PALETTE_RED, 1));
+  lv_obj_add_style(scr, &style, 0);
+}
 
 #if LV_USE_LOG != 0
 void my_print( lv_log_level_t level, const char * buf )
@@ -67,8 +91,10 @@ void my_print( lv_log_level_t level, const char * buf )
 }
 #endif
 
-/* LVGL calls it when a rendered image needs to copied to the display*/
-/* Not needed for TFT_eSPI, Bodmer already supplied a callback, this is not called */
+/* LVGL calls it when a rendered image needs to copied to the display.
+    Note this is not needed for TFT_eSPI created via the LVGL interface.
+    Bodmer supplies the rendering code. Left here for future reference.
+ */
 void my_disp_flush( lv_display_t *disp, const lv_area_t *area, uint8_t * px_map)
 {
     /*Copy `px map` to the `area`*/
@@ -92,7 +118,12 @@ void my_disp_flush( lv_display_t *disp, const lv_area_t *area, uint8_t * px_map)
     lv_display_flush_ready(disp);
 }
 
-/*Read the touchpad*/
+/* Read the touchpad coordinates. It appears that the 
+  TFT_eSPI touch coordinates are translated to the screen orientation.
+  That is, in portrait and landscape modes (0 and 90 rotation) upper left
+  is coordinate 0,0 (x,y). LVGL was modified in lv_indev.c to leave x,y
+  as is from TFT_eSPI getTouch call.
+ */
 void my_touchpad_read( lv_indev_t * indev, lv_indev_data_t * data )
 {
     uint16_t x, y;
@@ -102,15 +133,11 @@ void my_touchpad_read( lv_indev_t * indev, lv_indev_data_t * data )
         data->state = LV_INDEV_STATE_RELEASED;
     } else {
 #if 0
-        Serial.printf("x sent: %d y sent: %d\n", x, TFT_HOR_RES-y);
-        data->state = LV_INDEV_STATE_PRESSED;
-        data->point.x = x ;
-        data->point.y = TFT_HOR_RES - y;
-#endif
         Serial.printf("x actual: %d y actual: %d\n", x, y);
         Serial.println("Note: x and y are untranslated in lv_indev.c");
         Serial.printf("data.x = %d\n", x);
         Serial.printf("data.y = %d\n", y);
+#endif
         data->state = LV_INDEV_STATE_PRESSED;
         data->point.x =x ;
         data->point.y = y ;
@@ -129,26 +156,54 @@ static uint32_t my_tick(void)
 {
     return millis();
 }
-static void btn_event_cb(lv_event_t * event)
+static void main_button_event_cb(lv_event_t * event)
 {
-    lv_event_code_t code = lv_event_get_code(event);
-    lv_obj_t * label = lv_obj_get_child(btn1, 0);  // Label of button
+  lv_obj_t * button = lv_event_get_target_obj(event);
+  lv_event_code_t code = lv_event_get_code(event);
+  lv_obj_t * label = lv_obj_get_child(button, 0);  // Label of button
 
-    //Serial.printf("Button event is %d\n", code);
-    if(code == LV_EVENT_CLICKED) {
-        Serial.printf("Clicked\n");
-        if (strncmp(lv_label_get_text(label),"ON",2)==0) {
-            lv_label_set_text(label, "OFF");
-            lv_label_set_text(on_off_label, "OFF");
-        }
-        else {
-            lv_label_set_text(label, "ON");
-            lv_label_set_text(on_off_label, "ON");
-
-        }
+  //Serial.printf("Button event is %d\n", code);
+  // This is a checked button. 
+  // LV_EVENT_VALUE_CHANGED, LV_EVENT_VALUE_CLICKED
+  Serial.println("main button handler");
+  // Change back to start mode, if checked already
+  // I use strcmp instead of button state because timing of state change
+  // button callback is not deterministic in my experiements
+  if (code == LV_EVENT_VALUE_CHANGED) {
+    if (lv_strcmp(lv_label_get_text(label),"STOP")==0) {
+      lv_label_set_text(label,"START");
+      lv_label_set_text(mach_status_label,"Stopped...");
+    } 
+    else {
+      lv_label_set_text(label, "STOP");
+      lv_label_set_text(mach_status_label,"Running...");
     }
-
+  }
 }
+
+static void clean_spin_button_event_cb(lv_event_t * event)
+{
+  lv_obj_t * button = lv_event_get_target_obj(event);
+  lv_event_code_t code = lv_event_get_code(event);
+  lv_obj_t * label = lv_obj_get_child(button, 0);  // Label of button
+
+  //Serial.printf("Button event is %d\n", code);
+  // This is a checked button. 
+  // LV_EVENT_VALUE_CHANGED, LV_EVENT_VALUE_CLICKED
+  Serial.println("Clean/spin button handler");
+  // Change back to start mode, if checked already
+  // I use strcmp instead of button state because timing of state change
+  // button callback is not deterministic in my experiements
+  if (code == LV_EVENT_VALUE_CHANGED) {
+    if (lv_strcmp(lv_label_get_text(label),"CLEAN")==0) {
+      lv_label_set_text(label,"SPIN");
+    } 
+    else {
+      lv_label_set_text(label, "CLEAN");
+    }
+  }
+}
+#if 0
 static void slider_event_cb(lv_event_t * e)
 {
     //lv_obj_t * slider = lv_event_get_target_obj(e);
@@ -157,15 +212,16 @@ static void slider_event_cb(lv_event_t * e)
     lv_label_set_text_fmt(sl_label, "%" LV_PRId32, lv_slider_get_value(slider));
     lv_obj_align_to(sl_label, slider, LV_ALIGN_OUT_TOP_MID, 0, -15);    /*Align top of the slider*/
 }
+#endif
 
 
 void setup()
 {
-    String LVGL_Arduino = "Hello Arduino! ";
-    LVGL_Arduino += String('V') + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
+    String LVGL_Msg = "Watch Cleaner Controller";
+    LVGL_Msg += String('V') + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
 
     Serial.begin( 115200 );
-    Serial.println( LVGL_Arduino );
+    Serial.println( LVGL_Msg );
 
     lv_init();
 
@@ -179,16 +235,23 @@ void setup()
 
     lv_display_t * disp;
 #if LV_USE_TFT_ESPI
-    /*TFT_eSPI can be enabled lv_conf.h to initialize the display in a simple way*/
-    //disp = lv_tft_espi_create(TFT_VER_RES, TFT_HOR_RES, draw_buf, sizeof(draw_buf));
-    // Note modification of lv_tft_espi_create to accomodate TFT_ROTATION value
+    /* TFT_eSPI can be enabled lv_conf.h to initialize the display in a simple way
+      Note modification of lv_tft_espi_create to accomodate TFT_ROTATION value. This 
+      modification is in the LVGL library.
+      */
     disp = lv_tft_espi_create(SCREEN_WIDTH, SCREEN_HEIGHT, draw_buf, sizeof(draw_buf), TFT_ROTATION);
     lv_display_set_default(disp);
-    // This rotation is also required
+    // This rotation is also required to be coherent with the TFT_eSPI rotation.
     lv_display_set_rotation(disp, TFT_ROTATION);
+    // Add nice background to main screen
+    set_screen_bg_style(lv_screen_active());
+#if 0
     Serial.println("After lv_display_set_rotation");
     Serial.printf("disp->hor_res: %d  disp->ver_res: %d\n", 
       lv_display_get_horizontal_resolution(disp), lv_display_get_vertical_resolution(disp));
+#endif
+    
+    /* Manual screen touch calibration */
     touch_calibrate();
 #else
     /*Else create a display yourself*/
@@ -202,55 +265,96 @@ void setup()
     lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER); /*Touchpad should have POINTER type*/
     lv_indev_set_read_cb(indev, my_touchpad_read);
 
-    /* Create a simple label
-     * ---------------------
-     lv_obj_t *label = lv_label_create( lv_screen_active() );
-     lv_label_set_text( label, "Hello Arduino, I'm LVGL!" );
-     lv_obj_align( label, LV_ALIGN_CENTER, 0, 0 );
+    /* Create some global styles */
+    lv_style_init(&on_button_style);
+    lv_style_init(&off_button_style);
+    /* On button style */
+    lv_style_set_radius(&on_button_style, 3);
+    lv_style_set_bg_opa(&on_button_style, LV_OPA_100);
+    lv_style_set_bg_color(&on_button_style, lv_palette_main(LV_PALETTE_BLUE));
+    lv_style_set_border_opa(&on_button_style, LV_OPA_40);
+    lv_style_set_border_width(&on_button_style, 4);
+    lv_style_set_border_color(&on_button_style, lv_palette_main(LV_PALETTE_GREY));
+    //lv_style_set_shadow_width(&on_button_style, 4);
+    //lv_style_set_shadow_color(&on_button_style, lv_palette_main(LV_PALETTE_GREY));
+    //lv_style_set_shadow_offset_x(&on_button_style, 8);
+    //lv_style_set_shadow_offset_y(&on_button_style, 8);
+    lv_style_set_outline_opa(&on_button_style, LV_OPA_COVER);
+    lv_style_set_outline_color(&on_button_style, lv_color_black());
+    lv_style_set_outline_width(&on_button_style, 2);
+    lv_style_set_text_color(&on_button_style, lv_color_black());
+    lv_style_set_pad_all(&on_button_style, 10);
 
-     * Try an example. See all the examples
-     *  - Online: https://docs.lvgl.io/master/examples.html
-     *  - Source codes: https://github.com/lvgl/lvgl/tree/master/examples
-     * ----------------------------------------------------------------
+    /* Off button style*/
+    lv_style_set_radius(&off_button_style, 3);
+    lv_style_set_bg_opa(&off_button_style, LV_OPA_100);
+    lv_style_set_bg_color(&off_button_style, lv_palette_main(LV_PALETTE_GREY));
+    lv_style_set_border_opa(&off_button_style, LV_OPA_40);
+    lv_style_set_border_width(&off_button_style, 4);
+    lv_style_set_border_color(&off_button_style, lv_palette_darken(LV_PALETTE_GREY, 128));
+    //lv_style_set_shadow_width(&off_button_style, 4);
+    //lv_style_set_shadow_color(&off_button_style, lv_palette_main(LV_PALETTE_GREY));
+    //lv_style_set_shadow_offset_x(&off_button_style, 8);
+    //lv_style_set_shadow_offset_y(&off_button_style, 8);
+    lv_style_set_outline_opa(&off_button_style, LV_OPA_COVER);
+    lv_style_set_outline_color(&on_button_style, lv_color_black());
+    lv_style_set_outline_width(&on_button_style, 2);
+    lv_style_set_text_color(&off_button_style, lv_color_black());
+    lv_style_set_pad_all(&off_button_style, 10);
 
-     lv_example_btn_1();
 
-     * Or try out a demo. Don't forget to enable the demos in lv_conf.h. E.g. LV_USE_DEMO_WIDGETS
-     * -------------------------------------------------------------------------------------------
+    /* Header label */
+    lv_obj_t *main_header_label = lv_label_create( lv_screen_active() );
+    lv_label_set_text( main_header_label, "Watch Cleaner Controller, v" WCC_VER );
+    lv_obj_set_style_text_font(main_header_label, &lv_font_montserrat_20, 0);
+    lv_obj_align( main_header_label, LV_ALIGN_TOP_MID, 0, 0 );
 
-     lv_demo_widgets();
-     */
+    /* Start Button*/
+    lv_obj_t * start_button; 
+    start_button = lv_btn_create(lv_screen_active());
+    lv_obj_remove_style_all(start_button);
+    lv_obj_t * start_button_label = lv_label_create(start_button);
+    lv_label_set_text(start_button_label, "START");
+    lv_obj_center(start_button_label);
+    lv_obj_set_style_text_font(start_button_label, &lv_font_montserrat_18, 0);
+    lv_obj_add_flag(start_button, LV_OBJ_FLAG_CHECKABLE);
+    lv_obj_remove_flag(start_button, LV_OBJ_FLAG_PRESS_LOCK);
+    lv_obj_add_event_cb(start_button, main_button_event_cb, LV_EVENT_ALL, NULL);
+    lv_obj_set_width(start_button, 75); 
+    lv_obj_set_height(start_button,75);
+    lv_obj_align(start_button, LV_ALIGN_TOP_LEFT, 30, 85);
+    /* Apply styles */
+    lv_obj_add_style(start_button, &on_button_style, LV_STATE_DEFAULT);
+    lv_obj_add_style(start_button, &off_button_style, LV_STATE_CHECKED);
 
-    lv_obj_t *label = lv_label_create( lv_screen_active() );
-    lv_label_set_text( label, "Hello Arduino, I'm LVGL!" );
-    lv_obj_align( label, LV_ALIGN_TOP_MID, 0, 0 );
+    /* Clean/Spin Button*/
+    lv_obj_t * clean_spin_button; 
+    clean_spin_button = lv_btn_create(lv_screen_active());
+    lv_obj_remove_style_all(clean_spin_button);
+    lv_obj_t * clean_spin_button_label = lv_label_create(clean_spin_button);
+    lv_label_set_text(clean_spin_button_label, "CLEAN");
+    lv_obj_center(clean_spin_button_label);
+    lv_obj_set_style_text_font(clean_spin_button_label, &lv_font_montserrat_18, 0);
+    lv_obj_add_flag(clean_spin_button, LV_OBJ_FLAG_CHECKABLE);
+    lv_obj_remove_flag(clean_spin_button, LV_OBJ_FLAG_PRESS_LOCK);
+    lv_obj_add_event_cb(clean_spin_button, clean_spin_button_event_cb, LV_EVENT_ALL, NULL);
+    lv_obj_set_width(clean_spin_button, 75); 
+    lv_obj_set_height(clean_spin_button,75);
+    lv_obj_align_to(clean_spin_button, start_button, LV_ALIGN_OUT_RIGHT_MID, 15, 0);
 
-    /* Button*/
-    btn1 = lv_btn_create(lv_screen_active());
-    lv_obj_t * btn1_label = lv_label_create(btn1);
-    lv_label_set_text(btn1_label, "ON");
+    /* Apply styles */
+    lv_obj_add_style(clean_spin_button, &on_button_style, LV_STATE_DEFAULT);
+    lv_obj_add_style(clean_spin_button, &off_button_style, LV_STATE_CHECKED);
 
-    lv_obj_add_event_cb(btn1, btn_event_cb, LV_EVENT_ALL, NULL);
-    lv_obj_set_width(btn1, 50); //50
-    lv_obj_set_height(btn1,25);
-    //lv_obj_align(btn1, LV_ALIGN_CENTER, -80, -30);
-    //lv_obj_align(btn1, LV_ALIGN_CENTER, 80, -50);
-    lv_obj_align(btn1, LV_ALIGN_TOP_LEFT, 50, 150);
-    //lv_obj_align(btn1, LV_ALIGN_TOP_RIGHT, 0, 0);
-    //lv_obj_align(btn1, LV_ALIGN_BOTTOM_LEFT, 0, 0);
-    //lv_obj_align(btn1, LV_ALIGN_BOTTOM_RIGHT, 0, 0);
+    /* Machine status text label*/
+    mach_status_label = lv_label_create(lv_screen_active());
+    lv_obj_set_width(mach_status_label, 100);
+    lv_obj_set_height(mach_status_label, 25);
+    lv_obj_align(mach_status_label, LV_ALIGN_TOP_LEFT, 15, 215);
+    lv_label_set_text(mach_status_label, "Stopped..."); // Keyed by start button 
 
-    lv_obj_remove_flag(btn1, LV_OBJ_FLAG_PRESS_LOCK);
-
-    /* On/Off label modified by button click */
-    on_off_label = lv_label_create(lv_screen_active());
-    lv_obj_set_width(on_off_label, 50);
-    lv_obj_set_height(on_off_label, 25);
-    lv_obj_align(on_off_label, LV_ALIGN_TOP_MID, 0, 25);
-    lv_label_set_text(on_off_label, "ON"); // Start button value
-
+    #if 0
     /* Slider */
-    #if 1
     slider = lv_slider_create(lv_screen_active());
     lv_obj_set_width(slider, 200);               
     lv_obj_align(slider, LV_ALIGN_TOP_LEFT, 10, 100);
